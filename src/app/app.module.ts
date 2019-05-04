ccc7eb5fc9a0ea64f712939170398c405bdb48e4
sanjeep <sanjeep000@gmail.com>
2019-05-03 22:16:18 -0500

    dothis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { HttpClientService } from './service/http-client.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [HttpClientService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/service/http-client.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+export class Employee{
+  constructor(
+    public Id:string,
+    public name:string,
+    public lastName:string,
+    public designation:string,
+  ){}
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService {
+
+  constructor(private httpClient:HttpClient) 
+  { }
+
+ getEmployees()
+{
+console.log("test call");
+return this.httpClient.get<Employee[]>('http://localhost:8080/employees');
+}
+}
+
